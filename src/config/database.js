2ccbe4f1fc9048fd5b6a6caf2deed58249ac8158
unknown <Isadora Bom dos Santos>
2020-04-28 03:53:59 -0400

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'Rebom181909',
+    database: 'sqlnode',
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  };
